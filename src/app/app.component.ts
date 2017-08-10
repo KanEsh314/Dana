6b73e4edaccdfa9a2d3511e1b7522057de9ecf8f
KanEsh314 <kanesh314@gmail.com>
2017-08-10 23:30:31 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { CampaignPage } from '../pages/campaign/campaign';
 
 @Component({
   templateUrl: 'app.html'
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CampaignPage } from '../campaign/campaign';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,26 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    
   }
 
+  Campaign()
+  {
+    this.navCtrl.push(CampaignPage);
+  }
+
+  slides = [
+  {
+  	title : "Slider 1",
+  	description : "Welcome To My Dana",
+  },
+  {
+  	title : "Slider 2",
+  	description : "Welcome To My Dana",
+  },
+  {
+  	title : "Slider 3",
+  	description : "Welcome To My Dana",
+  }
+  ];
 }
